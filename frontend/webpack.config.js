96eaa58aa756fb642f9112fa5aefe164838c8f25
Nobime <romeo.nobime@mailo.com>
2022-07-18 15:08:48 +0200

    first commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.js",
+    polyfill: "babel-polyfill",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
